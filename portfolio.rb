0c895acb8f254c15f6068e6f614902efde47ad6b
Antoine Girard <thetoine@gmail.com>
2010-05-21 16:04:16 -0400

    last touch up, ready for deploy

--- portfolio.rb
+++ portfolio.rb
@@ -21,8 +21,47 @@ end
 helpers do
             
   def get_projects
-    data = File.open(PROJECTS_FILE).read    
-    return JSON.parse(data)
+    data = File.open(PROJECTS_FILE).read    	
+
+		# prepare array for project
+		projects = []		
+		
+		# create a Project instance for each project
+    JSON.parse(data).each do |project|			
+		 	projects << Project.new(project)
+		end
+		
+		# resort by name		
+		return projects.sort_by { |project| [project.date, project.name] }.reverse
   end
+
+	class Project
+
+		def initialize(data)
+			@data = data
+		end
+
+		def name
+			@data[1]['name']
+		end
+		
+		def client
+			@data[1]['client']
+		end
+		
+		def date
+			@data[1]['date']						
+		end
+		
+		def platform
+			@data[1]['platform']						
+		end
+		
+		def url
+			@data[1]['url']
+		end
+
+	end
   
 end
+
